7af1305ab08d0806645ce0fe0944ea965537a128
Cuong1114 <thaivietcuongductrong@gmail.com>
2025-03-07 12:58:28 +0700

    Cập nhật code mới

--- script.js
+++ script.js
@@ -18,10 +18,8 @@ async function submitData() {
     `;
     resultDisplay.style.color = 'green';
 
-    // Gửi dữ liệu đến Google Sheets thông qua proxy
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycby2Ku0jpN_C3NTeRAqKfVvl_mI2xKVIBuIlln0E0cy9JKXFX5EU6Y9hH8rKP89nr_127g/exec';
-    const url = proxyUrl + scriptUrl;
+    // Gửi dữ liệu đến Google Sheets
+    const url = 'https://script.google.com/macros/s/AKfycby2Ku0jpN_C3NTeRAqKfVvl_mI2xKVIBuIlln0E0cy9JKXFX5EU6Y9hH8rKP89nr_127g/exec';
     const data = {
         name: name,
         painLevel: painLevel
