340d9346be834135d84b9639f08774c08744ecd0
Cuong1114 <thaivietcuongductrong@gmail.com>
2025-03-07 10:56:36 +0700

    Cập nhật code mới

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function submitData() {
     resultDisplay.style.color = 'green';
 
     // Gửi dữ liệu đến Google Sheets
-    const url = 'https://script.google.com/macros/s/.../exec'; // Thay bằng URL của bạn
+    const url = 'https://script.google.com/macros/s/AKfycby2Ku0jpN_C3NTeRAqKfVvl_mI2xKVIBuIlln0E0cy9JKXFX5EU6Y9hH8rKP89nr_127g/exec';
     const data = {
         name: name,
         painLevel: painLevel
